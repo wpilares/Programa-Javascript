d0d600f25264b35983016c973e93cb3f848c4a2b
Will <wgpilaresc@gmail.com>
2022-02-16 18:41:41 -0500

    This is my first commit

--- Prueba.js
+++ Prueba.js
@@ -7,27 +7,29 @@ let nombre;
 while (a!="SALIR"){
 
   a=prompt("Ingrese su Nombre:");
+  if (a=="SALIR")
+  break;
   b=prompt("Ingrese su DNI:");
   nombres.push(a);
   dnis.push(b);
 }
-//Usamos esto para quitar los SALIR 
-nombres.pop();
-dnis.pop();
 
-let buscar = prompt("Indique nombre a buscar:");
+function buscador(buscar){
 
-
-if (nombres.includes(buscar)==true){
+  if (nombres.includes(buscar)==true){
   i=0;
   for (nombre of nombres){
     if(buscar==nombre){
       alert(`Nombre: ${nombre}`);
       alert(`DNI: ${dnis[i]}`);
     }
-    i=i+1;
+    i++;
   }
 }
 else{
   alert("Nombre no existe");
 }
+}
+
+let c = prompt("Indique nombre a buscar:");
+buscador(c);
